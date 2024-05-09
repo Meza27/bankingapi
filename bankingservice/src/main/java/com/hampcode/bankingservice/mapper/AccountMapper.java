a06e8b038713d0efb2b3375d12d6c4c6830e7a2a
Meza27 <u202224016@upc.edu.pe>
2024-05-08 21:23:00 -0500

    feat:Implementación de DTOs, mappers y excepciones

--- bankingservice/src/main/java/com/hampcode/bankingservice/mapper/AccountMapper.java
+++ bankingservice/src/main/java/com/hampcode/bankingservice/mapper/AccountMapper.java
@@ -1,4 +1,31 @@
 package com.hampcode.bankingservice.mapper;
 
+import com.hampcode.bankingservice.model.dto.AccountRequestDTO;
+import com.hampcode.bankingservice.model.dto.AccountResponseDTO;
+import com.hampcode.bankingservice.model.entity.Account;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@AllArgsConstructor
 public class AccountMapper {
+
+    private final ModelMapper modelMapper;
+
+    public Account converttoEntity(AccountRequestDTO accountRequestDTO) {
+        return modelMapper.map(accountRequestDTO, Account.class);
+    }
+
+    public AccountResponseDTO converttoDTO(Account account) {
+        return modelMapper.map(account, AccountResponseDTO.class);
+    }
+
+    public List<AccountResponseDTO> converttoDTO(List<Account> accounts) {
+        return accounts.stream()
+                .map(this::converttoDTO)
+                .toList();
+    }
 }
