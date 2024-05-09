846fda828ec099a7cb0a1e1135b2628f1822c736
Meza27 <u202224016@upc.edu.pe>
2024-05-08 20:33:53 -0500

    feat:Implementación de entidades

--- bankingservice/src/main/java/com/hampcode/bankingservice/model/dto/TransactionReportDTO.java
+++ bankingservice/src/main/java/com/hampcode/bankingservice/model/dto/TransactionReportDTO.java
@@ -1,4 +1,15 @@
 package com.hampcode.bankingservice.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TransactionReportDTO {
+    private LocalDate date;
+    private long transactionCount;
 }
--- bankingservice/src/main/java/com/hampcode/bankingservice/model/entity/Account.java
+++ bankingservice/src/main/java/com/hampcode/bankingservice/model/entity/Account.java
@@ -1,4 +1,33 @@
 package com.hampcode.bankingservice.model.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="accounts")
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="account_number", nullable=false, unique=true)
+    private String accountNumber;
+    @Column(name="balance", nullable=false)
+    private BigDecimal balance;
+    @Column(name="owner_name", nullable=false)
+    private String ownerName;
+    @Column(name="owner_email", nullable=false)
+    private String ownerEmail;
+    @Column(name="createdAt")
+    private LocalDate createdAt;
+    @Column(name="updatedAt")
+    private LocalDate updatedAt;
 }
